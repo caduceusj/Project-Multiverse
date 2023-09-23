870b5f6b52f2a093b24cabd9f1cf04f45c526d03
felipefausto42 <fellipe.fausto10@gmail.com>
2023-09-23 17:32:12 -0300

    right portal

--- Scripts/Scripts/phasePrefab.gd
+++ Scripts/Scripts/phasePrefab.gd
@@ -5,9 +5,10 @@ extends Node2D
 #@onready var spriteLeft = get_node("SpriteLeft")
 @onready var spriteLeft = $CharacterBody2D
 #@onready var spriteRight = get_node("SpriteRight")
-@onready var spriteRight = $SpriteRight
+@onready var spriteRight = $CharacterRight
 
 var isInArea = false
+var isInAreaRight = false
 var portalLeft = ""
 var portalRight = ""
 
@@ -18,7 +19,7 @@ func _ready():
 	portalLeft = PHASE_INFO.portalLeft
 	portalRight = PHASE_INFO.portalRight
 	spriteLeft.get_child(0).play(PHASE_INFO.leftEntity)
-	spriteRight.play(PHASE_INFO.rightEntity)
+	spriteRight.get_child(0).play(PHASE_INFO.rightEntity)
 	texture.texture = load(PHASE_INFO.background)
 	
 	
@@ -33,10 +34,17 @@ func _process(delta):
 	if(isInArea):
 		spriteLeft.velocity.x -= 5
 		spriteLeft.move_and_slide()
+		
+	if(isInAreaRight):
+		spriteRight.velocity.x += 5
+		spriteRight.move_and_slide()
+		
 func _on_right_portal_area_entered(area):
 	if(area.is_in_group("Player")):
 		State.currentPhase = portalRight
 		get_tree().reload_current_scene()
+	if(area.is_in_group("Target")):
+		area.get_parent().queue_free()
 
 
 func _on_left_portal_area_entered(area):
@@ -50,10 +58,20 @@ func _on_left_portal_area_entered(area):
 
 
 func _on_area_2d_area_entered(area):
-		if(area.is_in_group("Player")):
-			isInArea = true
+	if(area.is_in_group("Player")):
+		isInArea = true
 
 
 func _on_area_2d_area_exited(area):
-		if(area.is_in_group("Player")):
-			isInArea = false
+	if(area.is_in_group("Player")):
+		isInArea = false
+
+
+func _on_area_2d_right_area_entered(area):
+	if(area.is_in_group("Player")):
+		isInAreaRight = true
+
+
+func _on_area_2d_right_area_exited(area):
+	if(area.is_in_group("Player")):
+		isInAreaRight = false
