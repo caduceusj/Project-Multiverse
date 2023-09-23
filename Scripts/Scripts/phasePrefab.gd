54a9ef5746887fac2d4dcaf754f3972dc50e8ca8
felipefausto42 <fellipe.fausto10@gmail.com>
2023-09-23 17:09:43 -0300

    alright alright alright

--- Scripts/Scripts/phasePrefab.gd
+++ Scripts/Scripts/phasePrefab.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 @export var PHASE_INFO : Resource
 @onready var texture = get_node("TextureRect")
-@onready var spriteLeft = get_node("SpriteLeft")
-@onready var spriteRight = get_node("SpriteRight")
+#@onready var spriteLeft = get_node("SpriteLeft")
+@onready var spriteLeft = $CharacterBody2D
+#@onready var spriteRight = get_node("SpriteRight")
+@onready var spriteRight = $SpriteRight
 
-
+var isInArea = false
 var portalLeft = ""
 var portalRight = ""
 
@@ -15,7 +17,7 @@ func _ready():
 	PHASE_INFO = load("res://Scripts/Resources/"+str(State.currentPhase)+".tres")
 	portalLeft = PHASE_INFO.portalLeft
 	portalRight = PHASE_INFO.portalRight
-	spriteLeft.play(PHASE_INFO.leftEntity)
+	spriteLeft.get_child(0).play(PHASE_INFO.leftEntity)
 	spriteRight.play(PHASE_INFO.rightEntity)
 	texture.texture = load(PHASE_INFO.background)
 	
@@ -28,9 +30,9 @@ func _process(delta):
 	else:
 		$Morto.hide()
 	
-
-
-
+	if(isInArea):
+		spriteLeft.velocity.x -= 5
+		spriteLeft.move_and_slide()
 func _on_right_portal_area_entered(area):
 	if(area.is_in_group("Player")):
 		State.currentPhase = portalRight
@@ -41,3 +43,17 @@ func _on_left_portal_area_entered(area):
 	if(area.is_in_group("Player")):
 		State.currentPhase = portalLeft
 		get_tree().reload_current_scene()
+	if(area.is_in_group("Target")):
+		area.get_parent().queue_free()
+
+
+
+
+func _on_area_2d_area_entered(area):
+		if(area.is_in_group("Player")):
+			isInArea = true
+
+
+func _on_area_2d_area_exited(area):
+		if(area.is_in_group("Player")):
+			isInArea = false
