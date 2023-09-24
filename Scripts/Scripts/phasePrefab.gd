4251ccd3968bc9a71318a537037fa4a71ba34642
felipefausto42 <fellipe.fausto10@gmail.com>
2023-09-24 00:03:55 -0300

    I HAVE BECOME CONFORTABLY NUMB

--- Scripts/Scripts/phasePrefab.gd
+++ Scripts/Scripts/phasePrefab.gd
@@ -2,10 +2,9 @@ extends Node2D
 
 @export var PHASE_INFO : Resource
 @onready var texture = get_node("TextureRect")
-#@onready var spriteLeft = get_node("SpriteLeft")
-@onready var spriteLeft = $CharacterBody2D
-#@onready var spriteRight = get_node("SpriteRight")
+@onready var spriteLeft = $CharacterLeft
 @onready var spriteRight = $CharacterRight
+#@onready var 
 
 var isInArea = false
 var isInAreaRight = false
@@ -20,8 +19,16 @@ func _ready():
 	portalRight = PHASE_INFO.portalRight
 	spriteLeft.get_child(0).play(PHASE_INFO.leftEntity)
 	spriteRight.get_child(0).play(PHASE_INFO.rightEntity)
+	spriteLeft.scale.x = PHASE_INFO.spriteSideLeft
+	spriteRight.scale.x = PHASE_INFO.spriteSideRight
 	texture.texture = load(PHASE_INFO.background)
 	
+	if PHASE_INFO.world == 1:
+		$Nuvens.visible = true
+	elif PHASE_INFO.world == 2:
+		$Pantano.visible = true
+	elif PHASE_INFO.world == 3:
+		$Rico.visible = true
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -55,23 +62,19 @@ func _on_left_portal_area_entered(area):
 		area.get_parent().queue_free()
 
 
-
-
 func _on_area_2d_area_entered(area):
 	if(area.is_in_group("Player")):
 		isInArea = true
-
 
 func _on_area_2d_area_exited(area):
 	if(area.is_in_group("Player")):
 		isInArea = false
 
-
 func _on_area_2d_right_area_entered(area):
 	if(area.is_in_group("Player")):
 		isInAreaRight = true
 
-
 func _on_area_2d_right_area_exited(area):
 	if(area.is_in_group("Player")):
 		isInAreaRight = false
+		
--- Scripts/sceneSkeleton.gd
+++ Scripts/sceneSkeleton.gd
@@ -7,5 +7,8 @@ extends Resource
 @export var rightEntity = "NOME DO SPRITE"
 
 @export var background = "caminho para background"
+@export var spriteSideLeft = 1
+@export var spriteSideRight = 1
+@export var world = 0
 
 @export var willDie = false
